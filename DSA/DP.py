e36ba6c7203e36cc920eaea48875502a31efe05c
RahulKumar2340029 <karnrahul2001@outlook.com>
2024-06-11 10:23:34 +0530

    updated DP two problems

--- DSA/DP.py
+++ DSA/DP.py
@@ -6,6 +6,111 @@ from math import *
 from typing import *
 from sys import stdin
 
+"""2D Dynamic Programming """
+"""count all path from (0,0) to (n-1,m-1): https://www.naukri.com/code360/library/count-all-number-of-paths-of-a-given-matrix"""
+#using recursion
+def countPaths1(i,j)->int:
+    def f(m,n):
+        if m==0 and n==0:
+            return 1
+        """i,j - > i-1,j and i,j-1"""
+        if m<0 or n<0:
+            return 0
+        up = f(m-1,n)
+        left = f(m,n-1)
+        return up+left
+    return f(i-1,j-1)
+    pass
+#memo
+def countPath2(i, j):
+    memo = {}
+    def f(m,n):
+        if m==0 and n==0:
+            return 1
+        if m<0 or n<0:
+            return 0
+        #special memo condition
+        if (m,n) in memo:
+            return memo[(m,n)]
+        up = f(m-1,n)
+        left = f(m,n-1)
+        memo[(m,n)] = up + left
+        return memo[(m,n)]
+    return f(i-1,j-1)
+    pass
+#tabulation
+def countPath3(i, j):
+    dp = [[0 for _ in range(j + 1)] for _ in range(i + 1)]  # Corrected dimensions
+    for m in range(i + 1):
+        dp[m][0] = 0
+    for n in range(j + 1):
+        dp[0][n] = 0
+
+    dp[0][0] = 1
+    for m in range(1, i + 1):
+        for n in range(1, j + 1):
+            dp[m][n] = dp[m - 1][n] + dp[m][n - 1]
+
+    return dp[i][j]
+
+
+print(countPath3(3,2))
+
+
+"""https://www.naukri.com/code360/problems/ninja-s-training_3621003?leftPanelTabValue=PROBLEM"""
+
+#last performed for first is 3 which in no task performed as only 3 task are there with indices 0 1 2
+def ninjaTraining(n: int, points: List[List[int]]) -> int:
+    def f(index,last_performed):
+        #base condition 1
+        if index == 0:
+            maxi = -float('inf')
+            for i in range(0,3):
+                if i != last_performed:
+                    maxi = max(maxi,points[0][i])
+            return maxi
+
+        maxi = -float('inf')
+        for i in range(0,3):
+            if i != last_performed:
+                point = points[index][i] + f(index-1,i)
+                maxi = max(maxi,point)
+
+        return maxi
+    return f(n-1,3)
+
+    pass
+#memoization
+def ninjatraining(n: int, points: List[List[int]]) -> int:
+    memo = {}
+    def f(index,last):
+        if index == 0:
+            maxi = -float('inf')
+            for i in range(0, 3):
+                if i != last:
+                    maxi = max(maxi, points[0][i])
+            return maxi
+
+        #special memo condition
+        if (index,last) in memo:
+            return memo[(index,last)]
+        maxi = -float('inf')
+        for i in range(0, 3):
+            if i != last:
+                point = points[index][i] + f(index - 1, i)
+                maxi = max(maxi, point)
+            memo[(index,last)] = maxi
+        return memo[(index,last)]
+    return f(n-1,3)
+
+
+    pass
+#tabulation
+def ninjaTraning(n:int, points:List[List[int]])->int:
+    dp = [[0 for _ in range(4)] for _ in range(4)]
+
+    pass
+print(ninjatraining(3,[[1,2,5],[3,1,1],[3,3,3]]))
 """https://leetcode.com/problems/house-robber/description/"""
 """Memoization"""
 def rob(nums: List[int]) -> int:
