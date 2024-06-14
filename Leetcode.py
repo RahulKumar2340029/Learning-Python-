f69c8463cbe10938a5ea415daff607e4ea328702
RahulKumar2340029 <karnrahul2001@outlook.com>
2024-06-14 19:43:30 +0530

    Browser History

--- Leetcode.py
+++ Leetcode.py
@@ -148,13 +148,148 @@ def heightChecker( heights: List[int]) -> int:
 
     return count
 
+#leetcode day 11--6-2024 challenge
+def relativeSortArray(arr1: List[int], arr2: List[int]) -> List[int]:
+    ans = []
+    count = defaultdict(int)
+    for num in arr1:
+        count[num]+=1
+
+    for num in arr2:
+        while count[num] > 0:
+            ans.append(num)
+            count[num]-=1
+
+    for num in sorted(set(arr1) - set(arr2)):
+        while count[num] > 0:
+            ans.append(num)
+            count[num]-=1
+
+
+    return ans
+
+# leetcode day 12-06-2024 Dutch National Flag algo
+def sortColors(nums: List[int]) -> None:
+    low,mid,high = 0,0,len(nums)-1
+    while(mid <= high):
+        if nums[mid] == 0:
+            nums[mid],nums[low] = nums[low],nums[mid]
+            mid+=1
+            low+=1
+        elif nums[mid] == 1:
+            mid+=1
+        else:
+            nums[mid],nums[high] = nums[high],nums[mid]
+            high-=1
+
+arr = [2,0,2,1,1,0]
+sortColors(arr)
+print(arr)
+# leetcode day 13-06-2024
+"""https://leetcode.com/problems/minimum-number-of-moves-to-seat-everyone/description/"""
+# def minMovesToSeat(seats: List[int], students: List[int]) -> int:
+#     count_seats,count_students = [0]*100,[0]*100
+#     for ele in seats:
+
+# leetcode 14-06-2024
+def minIncrementForUnique(nums: List[int]) -> int:
+    nums.sort()
+    ans = 0
+    for i in range(1,len(nums)):
+        if nums[i] == nums[i-1]:
+            ans+=1
+            nums[i]+=1
+        elif nums[i] <  nums[i-1]:
+            to_make_equal = abs(nums[i] - nums[i-1])
+            ans+=to_make_equal+ 1
+            nums[i]+=to_make_equal + 1
+
+    return ans
+
+print(minIncrementForUnique([1,2,2]))
+# # 1 1 2 2 3 7
+"""class BrowserHistory:"""
+#     def __init__(self, homepage: str):
+#         self.st1 = [homepage];
+#         self.st2 = []
+#
+#     def visit(self, url: str) -> None:
+#         self.st2.clear()
+#         self.st1.append(url)
+#     def back(self, steps: int) -> str:
+#         while steps != 0 and len(self.st2)!=0:
+#             self.st2.append(self.st1.pop())
+#             steps-=1
+#         return self.st1.pop()
+#     def forward(self, steps: int) -> str:
+#         while steps!=0 and len(self.st2)!=0:
+#             self.st1.append(self.st2.pop())
+#             steps-=1
+#         self.st1.pop()
+
+"""Method2 very intuitive"""
+class Node:
+
+    def __init__(self,value:str):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+class BrowserHistory:
+
+    def __init__(self,homepage:str):
+        self.current = Node(homepage)
+
+    def visit(self, url: str) -> None:
+        new_node = Node(url)
+        self.current.next = new_node
+        new_node.prev = self.current
+        self.current = new_node
+
+    def back(self, steps: int) -> str:
+        while steps > 0 and self.current.prev is not None:
+            steps-=1
+            self.current = self.current.prev
+        return self.current.value
+
+    def forward(self, steps: int) -> str:
+        while steps > 0 and self.current.next is not None:
+            steps-=1
+            self.current = self.current.next
+
+        return self.current.value
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Your BrowserHistory object will be instantiated and called as such:
+# obj = BrowserHistory(homepage)
+# obj.visit(url)
+# param_2 = obj.back(steps)
+# param_3 = obj.forward(steps)
+
+
+
+
 
 
 
 
 
 if __name__ == '__main__':
-    heights = [1,2,3,5,4]
-    ans = heightChecker(heights)
-    print(ans)
+    # print(relativeSortArray([2,3,1,3,2,4,6,7,9,2,19],[2,1,4,3,9,6]))
     pass
