1a2235feaaea59fe920ad264e72e62db844fd306
RahulKumar2340029 <143934828+RahulKumar2340029@users.noreply.github.com>
2024-06-25 20:59:19 +0530

    CP

--- CP.py
+++ CP.py
@@ -1,6 +1,202 @@
+import math
 from typing import List
 import sys
 sys.setrecursionlimit(10**6)
+
+# def alice_and_books():
+#     n= int(input())
+#     pages = list(map(int,input().split()))
+#     print(pages[-1] + max(pages[:n-1]))
+#
+# for _ in range(int(input())):alice_and_books()
+# fs
+from collections import Counter
+
+
+def even_odd():
+    n, b = map(int, input().split())
+    arr = list(map(int, input().split()))
+
+    even_count = 0
+    odd_count = 0
+    mass = []
+
+    for i in range(n - 1):
+        if arr[i] % 2 == 0:
+            even_count += 1
+        else:
+            odd_count += 1
+
+        if even_count == odd_count:
+            mass.append(abs(arr[i] - arr[i + 1]))
+
+    mass.sort()
+    ans = 0
+
+    for diff in mass:
+        if diff <= b:
+            b -= diff
+            ans += 1
+        else:
+            break
+
+    print(ans)
+
+
+even_odd()
+
+
+# n = int(input().strip())
+#
+# k = 1
+# while k * (k + 1) // 2 <= n:
+#     k += 1
+# k -= 1
+# candies = []
+# total = 0
+# for i in range(1, k):
+#     candies.append(i)
+#     total += i
+# candies.append(n - total)
+# print(k)
+# print(" ".join(map(str, candies)))
+
+def sum_of_digits(n):
+    return sum(int(digit) for digit in str(n))
+
+
+def kth_perfect_number(k):
+    num = 19
+    count = 0
+
+    while count < k:
+        if sum_of_digits(num) == 10:
+            count += 1
+            if count == k:
+                return num
+        num+=9
+
+    return num
+# n = int(input().strip())
+# print(kth_perfect_number(n))
+
+
+# def kefa_and_first_steps():
+#     n = int(input())
+#     arr = list(map(int,input().split()))
+#     c, maxi = 1, 1
+#     for i in range(n-1):
+#         if(arr[i]<=arr[i+1]):
+#             c+=1
+#             maxi = max(c,maxi)
+#         else:
+#             c = 1
+#     print(maxi)
+#
+# kefa_and_first_steps()
+def update_queries():
+    a = list(map(int, input().split()))
+    b = list(input())
+    c = list(map(int, input().split()))
+    d = list(input())
+    d.sort()
+    c.sort()
+    d_pointer = 0
+
+    for i in range(a[1]):
+        if i < a[1] - 1:
+            if c[i + 1] == c[i]:
+                continue
+        b[c[i] - 1] = d[d_pointer]
+        d_pointer += 1
+    print(''.join(b))
+
+
+# for _ in range(int(input())):update_queries()
+"""max swaps in bubble sort n(n-1)/2 so even if one ele is sorted then less than n(n-1)/2 swaps needed"""
+def cubes_count():
+    n = int(input());
+    arr = list(map(int,input().split()))
+    for i in range(1,n):
+        if(arr[i]>=arr[i-1]):
+            print('YES')
+            return
+
+    print('NO')
+# for _ in range(int(input())):cubes_count()
+def forbidden_subsequence():
+    S = input()
+    T = input()
+    counts = Counter(S)
+    if T != 'abc':
+        print(str(sorted(S)))
+    elif 'a' not in S or 'b' not in S or 'c' not in S:
+        print(str(sorted(S)))
+    else:
+        output = ''.join([
+        'a' * counts['a'],'c' * counts['c'],'b' * counts['b'],''.join([char * counts[char] for char in sorted(counts.keys()) if char not in 'abc'])])
+        print("".join(output),type(output))
+
+# for _ in range(int(input())):forbidden_subsequence()
+
+# def gcd_problem():
+#     n= int(input());
+#     if (n&1)==0:
+#         print(f'{n-3} 2 1')
+#     else:
+#         print(f'{(n-1)//2 + 1} {(n-1)//2 - 1} 1' if ((n-1)//2)&1==0 else f'{(n-1)//2 + 2} {(n-1)//2 - 2} 1')
+#
+# for _ in range(int(input())):gcd_problem()
+# def bakery():
+#     n,a,b=map(int,input().split())
+#     if b <= a:
+#         max_profit = n * a
+#     else:
+#         max_profit = n * a
+#         cnt = min(n, b - a)
+#         max_profit -= cnt * a
+#         max_profit += cnt * (b + b - cnt + 1) // 2
+#     print(max_profit)
+# for _ in range(int(input())):bakery()
+
+#for n to be the power of 2
+"""i % n == i & (n-1) Very imp """
+# def f():
+#     pattern = [[0, 1, 1, 0], [1, 0, 0, 1],[1, 0, 0, 1],[0, 1, 1, 0]]
+#     n,m = map(int,input().split())
+#     for i in range(n):
+#         for j in range(m):
+#             print(pattern[i&3][j&3],end=" ")
+#         print()
+# for _ in range(int(input())):f()
+print(list(reversed([[1, 0, 0, 1],[0, 1, 1, 0]])))
+
+def solve():
+    n,m = map(int,input().split())
+    grid = []
+    maxi = {'r':0,'c':0}
+    col = {'f':0,'l':0}
+    for i in range(0,n):
+        row = input().strip()
+        count = row.count('#')
+        if count>maxi['c']:
+            maxi['c'] = count
+            maxi['r'] = i
+            col['f'] = row.find('#')
+            col['l'] = row.rfind('#')
+        grid.append(row)
+
+    rows = maxi['r']+1
+    cols = ((col['l']+col['f'])//2) + 1
+    print(f'{rows} {cols}')
+# T = int(input())
+# for _ in range(T):
+#     solve()
+
+
+
+
+
 
 def mydecorator(function):
 
@@ -139,20 +335,94 @@ def f(length,a,b,c):
     return maximum
 
 
-def solve():
+# def solve():
+#
+#     n, a, b, c = map(int, input().split())
+#     m = [0]*(n+1)
+#     m[0] = 0
+#     for i in range(1,n+1):
+#         maximum = max((m[i - a] + 1) if i - a >= 0 else -float('inf'),
+#                       (m[i - b] + 1) if i - b >= 0 else -float('inf'),
+#                       (m[i - c] + 1) if i - c >= 0 else -float('inf'))
+#         m[i] = maximum
+#
+#     print(m[n])
+#
+# solve()
+# for _ in range(int(input())): a, b = input().split(); print(f'{b[0] + a[1:]} {a[0] + b[1:]}')
 
-    n, a, b, c = map(int, input().split())
-    m = [0]*(n+1)
-    m[0] = 0
-    for i in range(1,n+1):
-        maximum = max((m[i - a] + 1) if i - a >= 0 else -float('inf'),
-                      (m[i - b] + 1) if i - b >= 0 else -float('inf'),
-                      (m[i - c] + 1) if i - c >= 0 else -float('inf'))
-        m[i] = maximum
+#
+def count_good_prefixes(a):
+    n = len(a)
+    prefix_sum = 0
+    good_prefix_count = 0
 
-    print(m[n])
+    for i in range(n):
+        prefix_sum += a[i]
+        # Check if there exists an element a[j] such that a[j] = prefix_sum - a[j]
+        for j in range(i + 1):
+            if a[j] == prefix_sum - a[j]:
+                good_prefix_count += 1
+                break  # We only need one such element to determine if the prefix is good
 
-solve()
+    return good_prefix_count
+
+print(count_good_prefixes([1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 294967296]))
+""" https://codeforces.com/contest/1985/problem/B Using most controversial Walrus operator := """
+# print(*[n if (n:=int(input())) <= 3 else 2 for _ in range(int(input()))], sep='\n')
+
+"""decimal to binary"""
+"""110001
+1100100"""
+# print(int('1100101',2) - int('101000',2))
+# print(bin(41)[2:])
+# print(bin(100)[2:])
+
+"""Bitwise"""
+"""Mocha math"""
+
+# def solve():
+#     n = int(input())
+#     arr = list(map(int,input().split()))
+#     rslt = arr[0]
+#     for num in range(1,n):
+#         rslt&=arr[num]
+#     print(rslt)
+#
+# T = int(input())
+# for _ in range(T):
+#     solve()
+
+"""E Secret box"""
+# def solve():
+#     x,y,z,k = map(int,input().split())
+#     ans = 0
+#     for i in range(1,x+1):
+#         for j in range(1,y+1):
+#             if k%(i*j)==0 and (p:=k//(i*j)) <= z: ans = max(ans,(x-i+1)*(y-j+1)*(z-p+1))
+#     print(ans)
+#
+#
+# for _ in range(int(input())):
+#     solve()
+
+
+# def solve():
+#     n = int(input())
+#     line = input().strip()
+#     numbers = list(map(int, line.split()))
+#     print(numbers[0])
+# T = int(input())
+# for _ in range(T):
+#     solve()
+
+
+
+
+
+
+
+
 
 
 
